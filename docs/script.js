cc83e62a80c1c9b11b2bc3f8c3e1e77bc1ec91a1
Shriya0-4 <shriyakulkarni04@gmail.com>
2025-04-22 14:33:15 +0530

    first commit

---
+++ docs/script.js
@@ -0,0 +1,12 @@
+function HandleSubmit(){
+  var username = document.getElementById("username").value;
+  var password = document.getElementById("password").value;
+  if(username==='admin' && password==='123'){
+  window.location.href="home.html";
+//  return false;
+  }
+  else{
+  alert("invalid user data!");
+//  return false;
+  }
+}
