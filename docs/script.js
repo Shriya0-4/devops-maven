1cc778ccaa81eca272a3b453f0fcee74da3e6b85
Shriya0-4 <shriyakulkarni04@gmail.com>
2025-04-25 09:44:41 +0530

    made changes in javascript file

--- docs/script.js
+++ docs/script.js
@@ -3,10 +3,10 @@ function HandleSubmit(){
   var password = document.getElementById("password").value;
   if(username==='admin' && password==='123'){
   window.location.href="home.html";
-//  return false;
+  return false;
   }
   else{
   alert("invalid user data!");
-//  return false;
+  return false;
   }
 }
